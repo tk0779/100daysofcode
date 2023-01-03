b8f6cd5d3e36a6c212873841c4519539563c1633
Nat TK <73867117+tk0779@users.noreply.github.com>
2023-01-03 20:29:02 +0700

    Add files via upload

---
+++ day48r1-slide/data.js
@@ -0,0 +1,25 @@
+const people = [
+    {
+      img:
+        "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959121/person-1_aufeoq.jpg",
+      name: "peter doe",
+      job: "product manager",
+      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis? `,
+    },
+    {
+      img:
+        "https://images.pexels.com/photos/1587009/pexels-photo-1587009.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      name: "susan doe",
+      job: "developer",
+      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+    },
+    {
+      img:
+        "https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      name: "Jack Henry",
+      job: "designer",
+      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+    },
+  ];
+  
+  export default people;
